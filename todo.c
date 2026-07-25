8aa54658b989f22f3d6b5b37e2f1cefb033d0d9f
ellanaqira <ellanrepo@gmail.com>
2026-07-25 17:16:47 +0700

    feat: major debunk at todo.c - add proper Task struct - remove previousfunction to create more proper function - add write data, read_data, store_to_struct, and str_compare function the reason why i rewrite this file is because the way of this code working has changed, the main change that i mean is at Task struct. and also i want to make the code more cleaner and organize based on those changes.

--- todo.c
+++ todo.c
@@ -1,79 +1,89 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 // constant
-#define MAXCHAR 1000
+#define MAXCHAR 256
 
 
 // Task structure
 typedef struct {
 	char todo[MAXCHAR];
-	char time[MAXCHAR];
-	char date[MAXCHAR];
+	float time;
+	int date;
+	int moth;
+	int years;
+	int status;
+	int mark;
 } Task;
 
 
-// function declarartion
-void add_task(Task *add, char *todo, char *time, char *date);
-void prtlargv(int argc, char *argv[]);
-int strcmpr(char str1[], char str2[]);
+
+// Function Declaration
+int write_data(Task data, char file_name[]);
+int read_data(Task data, char file_name[]);
+// Helper Function
+void store_to_struct(Task *data, char argv2[], char argv3[], char argv4[], char argv5[], char argv6[], char argv7[], char argv8[]);
+int str_compare(char str1[], char str2[]);
+
 
 
 // MAIN
 int main(int argc, char *argv[]) {
-	// Task container
-	Task add;
+	Task to_do;
 
-	if (strcmpr(argv[1], "add") == 1) {
-		if (argc < 5) {
-            printf("Error: add requires <task> <time> <date>\n");
+	if (str_compare(argv[1], "add") == 1) {
+		if (argc == 9) {
+			store_to_struct(&to_do, argv[2], argv[3], argv[4], argv[5], argv[6], argv[7], argv[8]);
+		}
+		else {
+			printf("'add' must be include <task> <time(hh.mm)> <date> <month> <years> <status> <mark>\n");
+		}
+	}
+}
+
+
+
+// Function Definition
+int write_data(Task data, char file_name[]) {
+    FILE *file = fopen(file_name, "ab");
+    if (file == NULL) {
+        printf("Failed to open file\n");
+        return 1;
+    }
+
+    // Write data to binary file
+    size_t num_written;
+    num_written = fwrite(&data, sizeof(data), 1, file);
+
+    if (num_written != 1) {
+        printf("Failed adding data to file\n");
+        return 1;
+    }
+    fclose(file);
+
+    printf("Successfully add data to file\n");
+    return 0;
+}
+
+void store_to_struct(Task *data, char argv2[], char argv3[], char argv4[], char argv5[], char argv6[], char argv7[], char argv8[]) {
+	strcpy(data->todo, argv2);
+	data->time = atof(argv3);
+	data->date = atoi(argv4);
+	data->moth = atoi(argv5);
+	data->years = atoi(argv6);
+	data->status = atoi(argv7);
+	data->mark = atoi(argv8);
+}
+
+int str_compare(char str1[], char str2[]) {
+    int i = 0;
+
+    while (str1[i] == str2[i]) {
+        if (str1[i] == '\0' && str2[i] == '\0') {
             return 1;
         }
-		add_task(&add, argv[2], argv[3], argv[4]);
-		prtlargv(argc, argv);
-	}
-
-	else {
-		printf("keyword '%s' not included\n", argv[1]);
-		prtlargv(argc, argv);
-	}
-	return 0;
-}
-
-
-void add_task(Task *add, char *todo, char *time, char *date) {
-	/*
-	Add task data to Task struct
-	*/
-	strcpy(add->todo, todo);
-	strcpy(add->time, time);
-	strcpy(add->date, date);
-}
-
-
-int strcmpr(char str1[], char str2[]) {
-	/*
-	Compare two string to check if it the same,
-	return 1 if two string are the same and
-	return 0 if otherwise.
-	*/
-	int i = 0;
-
-	while (str1[i] == str2[i]) {
-		if (str1[i] == '\0' && str2[i] == '\0') {
-			return 1;
-		}
-		i+=1;
-	}
-	return 0;
-}
-
-
-void prtlargv(int argc, char *argv[]) {
-	/*
-	Print list of argv line by line
-	*/
-	for (int i=0; i<argc; i+=1) {
-		printf("argv[%d] = %s\n", i, argv[i]);
-	}
+        ++i;
+    }
+    return 0;
 }
