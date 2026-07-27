b479a8c4f79c06e60a2becce2f9770685d805dcb
ellanaqira <ellanrepo@gmail.com>
2026-07-27 20:42:24 +0800

    feat: add print_help function to print information when argv[1] is help

--- todo.c
+++ todo.c
@@ -22,6 +22,7 @@ typedef struct {
 // Function Declaration
 int write_data(Task data, char file_name[]);
 int read_data(Task data, char file_name[]);
+void print_help(void);
 // Helper Function
 void store_to_struct(Task *data, char argv2[], char argv3[], char argv4[], char argv5[], char argv6[], char argv7[], char argv8[]);
 int str_compare(char str1[], char str2[]);
@@ -32,7 +33,11 @@ int str_compare(char str1[], char str2[]);
 int main(int argc, char *argv[]) {
 	Task to_do;
 
-	if (str_compare(argv[1], "add") == 1) {
+    if (str_compare(argv[1], "help") == 1) {
+        print_help();
+    }
+
+	else if (str_compare(argv[1], "add") == 1) {
 		if (argc == 9) {
 			store_to_struct(&to_do, argv[2], argv[3], argv[4], argv[5], argv[6], argv[7], argv[8]);
 			write_data(to_do, "file.bin");
@@ -51,11 +56,21 @@ int main(int argc, char *argv[]) {
 		printf("todo: '%s' is not a todo command.\n", argv[1]);
 		return 1;
 	}
+
+    return 0;
 }
 
 
 
 // Function Definition
+void print_help(void){
+    printf("[ Help ]\n");
+    printf("Command:\n");
+    printf("    add     to store task and write it to binary file\n");
+    printf("    show    print out stored task\n");
+}
+
+
 int write_data(Task data, char file_name[]) {
     FILE *file = fopen(file_name, "ab");
     if (file == NULL) {
