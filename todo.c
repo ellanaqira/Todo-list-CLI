e97dbf667cb1fa7261543ffd96c06e274078d0a1
ellanaqira <ellanrepo@gmail.com>
2026-07-31 20:44:14 +0800

    feat: add more information for help command

--- todo.c
+++ todo.c
@@ -87,6 +87,9 @@ void print_help(void){
     printf("[ Help ]\n");
     printf("Command:\n");
     printf("    add     to store task and write it to binary file\n");
+    printf("            'add' must be include <task> <time(hh.mm)> <date> <month> <years> <status> <mark>\n");
+    printf("            <status> : 0 = not at all urgent, 1 = not urgent, 2 = quite urgent, 3 = urgent\n");
+    printf("            <mark>   : 0 = not finish, 1 = finish\n");
     printf("    show    print out stored task\n");
 }
 
